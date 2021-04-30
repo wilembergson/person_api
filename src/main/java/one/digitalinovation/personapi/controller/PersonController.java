0afafae5af954f12cf9e5493fc8501c77dc0d734
wilembergson <wilembergson.yahoo.com>
2021-04-30 12:12:07 -0300

    Creating services

--- src/main/java/one/digitalinovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinovation/personapi/controller/PersonController.java
@@ -1,15 +1,27 @@
 package one.digitalinovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinovation.personapi.dto.MessageResponseDTO;
+import one.digitalinovation.personapi.entity.Person;
+import one.digitalinovation.personapi.repository.PersonRepository;
+import one.digitalinovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
-    //aaaaaaa
-    @GetMapping
-    public String getBook(){
-        return "API test!";
+
+    private PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        return personService.createPerson(person);
     }
 }
---
+++ src/main/java/one/digitalinovation/personapi/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package one.digitalinovation.personapi.repository;
+
+import one.digitalinovation.personapi.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
